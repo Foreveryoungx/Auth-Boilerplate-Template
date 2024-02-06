c3f69695e3380d21cacb6c3fd7147d9a08fa4803
Trey Allen <trevon.allen1994@gmail.com>
2024-02-05 23:26:20 -0600

    updated schema.prisma and login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -34,8 +34,9 @@ export const LoginForm = () => {
         startTransition(() => {
             login(values)
                 .then((data) => {
-                    setError(data.error);
-                    setSuccess(data.success);
+                    setError(data?.error);
+                    // @ts-ignore
+                    setSuccess(data?.success);
                 })
         });
     }
