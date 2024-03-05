238c0174517bd560bb5a518d111a9e5744e784ff
Trey Allen <trevon.allen1994@gmail.com>
2024-03-04 18:50:16 -0600

    Email verification setup complete

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,9 @@
+import {NewVerificationForm} from '@/components/auth/new-verfication-form';
+
+const NewVerificationPage = () => {
+    return (
+        <NewVerificationForm/>
+    )
+}
+
+export default NewVerificationPage;
